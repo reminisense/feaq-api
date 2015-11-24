806a151056f642f457e55b42b119b7b91738fe30
Aunne Arzadon <nodazra@gmail.com>
2015-11-24 11:59:07 +0800

    ARA API Documentation ARA 11242015 Added documentation comments on the functions I created

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -14,14 +14,106 @@ use Illuminate\Support\Facades\Input;
 class AuthenticationController extends Controller
 {
 
+    /**
+     * @api {post} /login
+     * @apiName login
+     * @apiGroup Authentication
+     * @apiVersion 1.0.0
+     * @apiExample {js} Example Usage
+     *      http://api.featherq.com/login
+     * @apiDescription Logs in a user into the api and returns an access key
+     *
+     * @apiHeader none
+     * @apiPermission none
+     *
+     * @apiParam {String} fb_id User's facebook id provided by the Facebook javascript api
+     *
+     * @apiSuccess (Success 200) {String} success The status of the request
+     * @apiSuccess (Success 200) {String} accessToken The accessKey to remember the session
+     * @apiSuccessExample {Json} Success-Response:
+     *      HTTP/1.1 200 OK
+     *      {
+     *          "success": 1,
+     *          "accessToken": "123123drink123123drink"
+     *      }
+     *
+     * @apiError (Error 404) {String} success Returns 0 success if failed
+     * @apiError (Error 404) {String} error The body of the error message
+     * @apiErrorExample {Json} Error-response:
+     *      HTTP/1.1 404 Not Found
+     *      {
+     *          "success": 0,
+     *          "error": "Please sign up to Featherq"
+     *      }
+     *
+     */
     public function login(){
         return Authentication::login(Input::get('fb_id'));
     }
 
+    /**
+     * @api {get} /logout
+     * @apiName logout
+     * @apiGroup Authentication
+     * @apiVersion 1.0.0
+     * @apiExample {js} Example Usage
+     *      http://api.featherq.com/logout
+     * @apiDescription Logs out a user from the api
+     *
+     * @apiHeader none
+     * @apiPermission none
+     *
+     * @apiSuccess (Success 200) {String} success The status of the request
+     * @apiSuccessExample {Json} Success-Response:
+     *      HTTP/1.1 200 OK
+     *      {
+     *          "success": 1,
+     *      }
+     *
+     */
     public function logout(){
         return Authentication::logout();
     }
 
+    /**
+     * @api {post} /user/register
+     * @apiName register
+     * @apiGroup Authentication
+     * @apiVersion 1.0.0
+     * @apiExample {js} Example Usage
+     *      http://api.featherq.com/user/register
+     * @apiDescription Registers a new user to the database
+     *
+     * @apiHeader none
+     * @apiPermission none
+     *
+     * @apiParam {String} accessToken The facebook access token (provided by the Facebook javascript api)
+     * @apiParam {String} fb_id User's facebook id (provided by the Facebook javascript api)
+     * @apiParam {String} fb_url User's facebook url (provided by the Facebook javascript api)
+     * @apiParam {String} first_name User's first name (provided by the Facebook javascript api)
+     * @apiParam {String} last_name User's last name (provided by the Facebook javascript api)
+     * @apiParam {String} email User's email (provided by the Facebook javascript api)
+     * @apiParam {String} gender User's gender (provided by the Facebook javascript api)
+     *
+     * @apiSuccess (Success 200) {String} success The status of the request
+     * @apiSuccess (Success 200) {String} accessToken The accessKey to remember the session
+     * @apiSuccessExample {Json} Success-Response:
+     *      HTTP/1.1 200 OK
+     *      {
+     *          "success": 1,
+     *          "accessToken": "123123drink123123drink"
+     *      }
+     *
+     * @apiError (Error 404) {String} success Returns 0 success if failed
+     * @apiError (Error 404) {String} error The body of the error message
+     * @apiErrorExample {Json} Error-response:
+     *      HTTP/1.1 404 Not Found
+     *      {
+     *          "success": 0,
+     *          "error": "Please sign up to Featherq"
+     *      }
+     *
+     */
     public function register(){
         return Authentication::register(Input::all());
     }
--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -12,11 +12,89 @@ use App\Models\Business;
 
 class BusinessController extends Controller
 {
+    /**
+     * @api {get} /business/search
+     * @apiName business search
+     * @apiGroup Business
+     * @apiVersion 1.0.0
+     * @apiExample {js} Example Usage
+     *      http://api.featherq.com/business/search?keyword=&country=&industry=&time_open=&timezone=&limit=&offset
+     * @apiDescription Search for businesses based on the given parameters
+     *
+     * @apiHeader none
+     * @apiPermission none
+     *
+     * @apiParam {String} [keyword] The keyword used to search for the business
+     * @apiParam {String} [country] The country of the business
+     * @apiParam {String} [industry] The industry of the business
+     * @apiParam {String} [time_open] The time the business opens
+     * @apiParam {String} [timezone] The timezone of the business
+     * @apiParam {Number} [limit] The number of entries to be retrieved
+     * @apiParam {Number} [offset] The number where the entries retrieved will start
+     *
+     * @apiSuccess (Success 200) {Object} arr Array of objects with business details
+     * @apiSuccess (Success 200) {Number} arr.business_id The business id of the business in the database
+     * @apiSuccess (Success 200) {String} arr.business_name The name of the business
+     * @apiSuccess (Success 200) {String} arr.local_address The address of the business
+     * @apiSuccess (Success 200) {String} arr.time_open The time that the business opens
+     * @apiSuccess (Success 200) {String} arr.time_close The time that the business closes
+     * @apiSuccess (Success 200) {String} arr.waiting_time Indicates how heavy the queue is based on time it takes for the last number in the queue to be called
+     * @apiSuccess (Success 200) {Number} arr.last_number_called The last number called by the business
+     * @apiSuccess (Success 200) {Number} arr.next_available_number The next number that can be placed to the queue
+     * @apiSuccess (Success 200) {Number} arr.last_active The number of days when the business last processed the queue
+     * @apiSuccess (Success 200) {Bool} arr.card_bool Indicates if the business is active or not
+     *
+     * @apiSuccessExample {Json} Success-Response:
+     *      HTTP/1.1 200 OK
+     *      {
+     *          [
+     *              "business_id": 1,
+     *              "business_name": "Angel's Burger",
+     *              "local_address": "Hernan Cortes st. Subangdako, Mandaue City",
+     *              "time_open": "10:00 AM",
+     *              "time_close": "4:00 PM",
+     *              "waiting_time": "light",
+     *              "last_number_called": "none",
+     *              "next_available_number": 1,
+     *              "last_active": 5,
+     *              "card_bool": false
+     *          ]
+     *      }
+     *
+     */
     public function search(){
         $arr = Business::searchBusiness($_GET);
         return json_encode($arr);
     }
 
+    /**
+     * @api {get} /business/search-suggest/{keyword}
+     * @apiName business search-suggest
+     * @apiGroup Business
+     * @apiVersion 1.0.0
+     * @apiExample {js} Example Usage
+     *      http://api.featherq.com/business/search-suggest/keyword
+     * @apiDescription Suggests search items for businesses based on the given keyword
+     *
+     * @apiHeader none
+     * @apiPermission none
+     *
+     * @apiParam {String} [keyword] The keyword used to search for the business
+     *
+     * @apiSuccess (Success 200) {Object} arr Array of objects with business details
+     * @apiSuccess (Success 200) {String} arr.business_name The name of the business
+     * @apiSuccess (Success 200) {String} arr.local_address The address of the business
+     *
+     * @apiSuccessExample {Json} Success-Response:
+     *      HTTP/1.1 200 OK
+     *      {
+     *          [
+     *              "business_name": "Angel's Burger",
+     *              "local_address": "Hernan Cortes st. Subangdako, Mandaue City",
+     *          ]
+     *      }
+     *
+     */
     public function searchSuggest($keyword){
         $businesses = Business::searchSuggest($keyword);
         return json_encode(array('keywords' => $businesses));
