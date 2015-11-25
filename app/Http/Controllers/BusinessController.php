a75d6516f117f8234b100196f5052efde75cf6b1
Jonas Clyde Almocera <jonas@reminisense.com>
2015-11-25 18:28:44 +0800

    JCA Modified Codes 11252015 - Edited some of the annotations. - Edited variables.

--- app/Http/Controllers/BusinessController.php
+++ app/Http/Controllers/BusinessController.php
@@ -13,36 +13,36 @@ use App\Models\Business;
 class BusinessController extends Controller
 {
     /**
-     * @api {get} /business/search Search Business
-     * @apiName business search
+     * @api {get} /business/search Search Businesses
+     * @apiName Search
      * @apiGroup Business
      * @apiVersion 1.0.0
      * @apiExample {js} Example Usage
      *      http://api.featherq.com/business/search?keyword=&country=&industry=&time_open=&timezone=&limit=&offset
-     * @apiDescription Search for businesses based on the given parameters
+     * @apiDescription Search for businesses based on the given parameters.
      *
      * @apiHeader none
      * @apiPermission none
      *
-     * @apiParam {String} [keyword] The keyword used to search for the business
-     * @apiParam {String} [country] The country of the business
-     * @apiParam {String} [industry] The industry of the business
-     * @apiParam {String} [time_open] The time the business opens
-     * @apiParam {String} [timezone] The timezone of the business
-     * @apiParam {Number} [limit] The number of entries to be retrieved
-     * @apiParam {Number} [offset] The number where the entries retrieved will start
+     * @apiParam {String} [keyword] The keyword used to search for the business.
+     * @apiParam {String} [country] The country of the business.
+     * @apiParam {String} [industry] The industry of the business.
+     * @apiParam {String} [time_open] The time the business opens.
+     * @apiParam {String} [timezone] The timezone of the business.
+     * @apiParam {Number} [limit] The maximum number of entries to be retrieved.
+     * @apiParam {Number} [offset] The number where the entries retrieved will start.
      *
-     * @apiSuccess (200) {Object} business Array of objects with business details
-     * @apiSuccess (200) {Number} business.business_id The business id of the business in the database
-     * @apiSuccess (200) {String} business.business_name The name of the business
-     * @apiSuccess (200) {String} business.local_address The address of the business
-     * @apiSuccess (200) {String} business.time_open The time that the business opens
-     * @apiSuccess (200) {String} business.time_close The time that the business closes
-     * @apiSuccess (200) {String} business.waiting_time Indicates how heavy the queue is based on time it takes for the last number in the queue to be called
-     * @apiSuccess (200) {Number} business.last_number_called The last number called by the business
-     * @apiSuccess (200) {Number} business.next_available_number The next number that can be placed to the queue
-     * @apiSuccess (200) {Number} business.last_active The number of days when the business last processed the queue
-     * @apiSuccess (200) {Bool} business.card_bool Indicates if the business is active or not
+     * @apiSuccess (200) {Object[]} business Array of objects with business details.
+     * @apiSuccess (200) {Number} business.business_id The business id of the retrieved business from the database.
+     * @apiSuccess (200) {String} business.business_name The name of the business.
+     * @apiSuccess (200) {String} business.local_address The address of the business.
+     * @apiSuccess (200) {String} business.time_open The time that the business opens.
+     * @apiSuccess (200) {String} business.time_close The time that the business closes.
+     * @apiSuccess (200) {String} business.waiting_time Indicates how heavy the queue is based on time it takes for the last number in the queue to be called.
+     * @apiSuccess (200) {Number} business.last_number_called The last number called by the business.
+     * @apiSuccess (200) {Number} business.next_available_number The next number that can be placed to the queue.
+     * @apiSuccess (200) {Number} business.last_active The number of days when the business last processed the queue.
+     * @apiSuccess (200) {Boolean} business.card_bool Indicates if the business is active or not.
      *
      * @apiSuccessExample {Json} Success-Response:
      *      HTTP/1.1 200 OK
@@ -69,7 +69,7 @@ class BusinessController extends Controller
 
     /**
      * @api {get} /business/search-suggest/{keyword} Search Suggestion
-     * @apiName business search-suggest
+     * @apiName SearchSuggest
      * @apiGroup Business
      * @apiVersion 1.0.0
      * @apiExample {js} Example Usage
@@ -79,11 +79,11 @@ class BusinessController extends Controller
      * @apiHeader none
      * @apiPermission none
      *
-     * @apiParam {String} [keyword] The keyword used to search for the business
+     * @apiParam {String} keyword The keyword used to search for the business
      *
-     * @apiSuccess (200) {Object} arr Array of objects with business details
-     * @apiSuccess (200) {String} arr.business_name The name of the business
-     * @apiSuccess (200) {String} arr.local_address The address of the business
+     * @apiSuccess (200) {Object[]} business Array of objects with business details
+     * @apiSuccess (200) {String} business.business_name The name of the business
+     * @apiSuccess (200) {String} business.local_address The address of the business
      *
      * @apiSuccessExample {Json} Success-Response:
      *      HTTP/1.1 200 OK
@@ -97,6 +97,6 @@ class BusinessController extends Controller
      */
     public function searchSuggest($keyword){
         $businesses = Business::searchSuggest($keyword);
-        return json_encode(array('keywords' => $businesses));
+        return json_encode(array('businesses' => $businesses));
     }
 }
