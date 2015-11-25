b72aee970c3b0ecc395d84570b2c0ad831912104
Jonas Clyde Almocera <jonas@reminisense.com>
2015-11-25 21:34:16 +0800

    JCA Added Periods 11252015 -Added periods for documentation.

--- app/Http/Controllers/AuthenticationController.php
+++ app/Http/Controllers/AuthenticationController.php
@@ -16,21 +16,21 @@ class AuthenticationController extends Controller
 
     /**
      * @api {post} /login User Login
-     * @apiName login
+     * @apiName Login
      * @apiGroup Authentication
      * @apiVersion 1.0.0
      * @apiExample {js} Example Usage
      *      http://api.featherq.com/login
-     * @apiDescription Checks for the user in the database and returns an access key
+     * @apiDescription Checks for the user in the database and returns an access key.
      *
      * @apiHeader none
      * @apiPermission none
      *
-     * @apiParam {String} fb_id User's facebook id provided by the Facebook javascript api
-     * @apiParam {String} click_source the button where the user logged in to featherq
+     * @apiParam {String} fb_id User's Facebook id provided by the Facebook javascript API.
+     * @apiParam {String} click_source The location of the button (e.g. landing_page_top_right), where the user logged in to Featherq.
      *
-     * @apiSuccess (200) {String} success The status of the request
-     * @apiSuccess (200) {String} accessToken The accessKey to remember the session
+     * @apiSuccess (200) {String} success Returns <code>1</code> if the login is successful.
+     * @apiSuccess (200) {String} accessToken The access key to remember the session.
      * @apiSuccessExample {Json} Success-Response:
      *      HTTP/1.1 200 OK
      *      {
@@ -38,13 +38,13 @@ class AuthenticationController extends Controller
      *          "accessToken": "123123drink123123drink"
      *      }
      *
-     * @apiError (200) {String} success Returns 0 success if failed
-     * @apiError (200) {String} error The body of the error message
+     * @apiError (200) {String} success Returns <code>0</code> if the login fails.
+     * @apiError (200) {String} err_message The body of the error message.
      * @apiErrorExample {Json} Error-response:
      *      HTTP/1.1 200
      *      {
      *          "success": 0,
-     *          "error": "Please sign up to Featherq"
+     *          "err_message": "Please sign up to Featherq"
      *      }
      *
      */
@@ -54,21 +54,21 @@ class AuthenticationController extends Controller
 
     /**
      * @api {get} /logout User Logout
-     * @apiName logout
+     * @apiName Logout
      * @apiGroup Authentication
      * @apiVersion 1.0.0
      * @apiExample {js} Example Usage
      *      http://api.featherq.com/logout
-     * @apiDescription Forgets the user's session from the app
+     * @apiDescription Forgets the user's session from the app.
      *
      * @apiHeader none
      * @apiPermission none
      *
-     * @apiSuccess (200) {String} success The status of the request
+     * @apiSuccess (200) {String} success Returns <code>1</code> if the login is successful.
      * @apiSuccessExample {Json} Success-Response:
      *      HTTP/1.1 200 OK
      *      {
-     *          "success": 1,
+     *          "success": 1
      *      }
      *
      */
@@ -76,29 +76,30 @@ class AuthenticationController extends Controller
         return Authentication::logout();
     }
 
+
     /**
      * @api {post} /user/register User Registration
-     * @apiName register
+     * @apiName Register
      * @apiGroup Authentication
      * @apiVersion 1.0.0
      * @apiExample {js} Example Usage
      *      http://api.featherq.com/user/register
-     * @apiDescription Registers a new user to the database
+     * @apiDescription Registers a new user to the database.
      *
      * @apiHeader none
      * @apiPermission none
      *
-     * @apiParam {String} accessToken The facebook access token (provided by the Facebook javascript api)
-     * @apiParam {String} fb_id User's facebook id (provided by the Facebook javascript api)
-     * @apiParam {String} fb_url User's facebook url (provided by the Facebook javascript api)
-     * @apiParam {String} first_name User's first name (provided by the Facebook javascript api)
-     * @apiParam {String} last_name User's last name (provided by the Facebook javascript api)
-     * @apiParam {String} email User's email (provided by the Facebook javascript api)
-     * @apiParam {String} gender User's gender (provided by the Facebook javascript api)
-     * @apiParam {String} click_source the button where the user logged in to featherq
+     * @apiParam {String} accessToken The facebook access token (provided by the Facebook javascript API).
+     * @apiParam {String} fb_id User's facebook id (provided by the Facebook javascript API).
+     * @apiParam {String} fb_url User's facebook url (provided by the Facebook javascript API).
+     * @apiParam {String} first_name User's first name (provided by the Facebook javascript API).
+     * @apiParam {String} last_name User's last name (provided by the Facebook javascript API).
+     * @apiParam {String} email User's email (provided by the Facebook javascript API).
+     * @apiParam {String} gender User's gender (provided by the Facebook javascript API).
+     * @apiParam {String} click_source The location of the button (e.g. landing_page_top_right), where the user logged in to Featherq.
      *
-     * @apiSuccess (200) {String} success The status of the request
-     * @apiSuccess (200) {String} accessToken The accessKey to remember the session
+     * @apiSuccess (200) {String} success Returns <code>1</code> if the sign up is successful.
+     * @apiSuccess (200) {String} accessToken The access key to remember the session.
      * @apiSuccessExample {Json} Success-Response:
      *      HTTP/1.1 200 OK
      *      {
