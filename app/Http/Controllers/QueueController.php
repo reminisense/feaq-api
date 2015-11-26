6b7d81f0bb944a39219a80ffb57759e94c3ea9c3
polljii <paul@reminisense.com>
2015-11-26 16:11:22 +0800

    PAG usort Function Parameter Fix
    - usort params were not implemented correctly

--- app/Http/Controllers/QueueController.php
+++ app/Http/Controllers/QueueController.php
@@ -261,8 +261,8 @@ class QueueController extends Controller {
         }
       }
 
-      usort($processed_numbers, $this->sortProcessedNumbers());
-      usort($called_numbers, $this->sortCalledNumbers());
+      usort($processed_numbers, array($this, 'sortProcessedNumbers'));
+      usort($called_numbers, array($this, 'sortCalledNumbers'));
 
       $priority_numbers->last_number_given = $last_number_given;
       $priority_numbers->next_number = $this->nextNumber($priority_numbers->last_number_given, QueueSettings::numberStart($service_id), QueueSettings::numberLimit($service_id));
