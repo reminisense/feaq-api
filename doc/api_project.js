af9a4b4bbdb160aa49e402ade70ce9d85d83c2e5
Jonas Clyde Almocera <jonas@reminisense.com>
2015-11-30 19:47:42 +0800

    JCA Added APIDOC 11302015 -Added all the necessary files needed for APIDOCs.

---
+++ doc/api_project.js
@@ -0,0 +1,15 @@
+define({
+  "name": "FeatherQ APIs",
+  "version": "0.1.0",
+  "description": "This is the official documentation of the FeatherQ APIs.",
+  "title": "FeatherQ - APIs",
+  "url": "https://api.featherq.com/",
+  "sampleUrl": false,
+  "apidoc": "0.2.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2015-11-30T11:19:42.883Z",
+    "url": "http://apidocjs.com",
+    "version": "0.13.1"
+  }
+});
